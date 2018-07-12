c14027d2c73600a6da2acf2a16da2b4040814d51
Luke Bakken <luke@bakken.io>
2018-07-12 15:09:13 -0700

    v2

--- erl_records.erl
+++ erl_records.erl
@@ -4,15 +4,18 @@
          parse/1]).
 
 -record(test, {
-          version = 1,
+          version = 2,
           f1 = 0,
           f2 = f2_atom,
           f3 = {foo, bar, baz},
           f4 = "yo mama"
+          f5 = #{}
          }).
 
 new() ->
     #test{}.
 
+parse(#test{version = 2, f1 = F1, f2 = F2, f3 = F3, f4 = F4, f5 = F5}) ->
+    [{f1, F1}, {f2, F2}, {f3, F3}, {f4, F4}, {f5, F5}];
 parse(#test{version = 1, f1 = F1, f2 = F2, f3 = F3}) ->
     [{f1, F1}, {f2, F2}, {f3, F3}].
