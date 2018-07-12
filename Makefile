ec061bededf96e6c17ae0cc35a5634b832df0191
Luke Bakken <luke@bakken.io>
2018-07-12 15:33:28 -0700

    v2

---
+++ Makefile
@@ -0,0 +1,2 @@
+erl_records.beam: erl_records.erl
+	erlc +debug_info erl_records.erl
--- erl_records.erl
+++ erl_records.erl
@@ -1,6 +1,7 @@
 -module(erl_records).
 
 -export([new/0,
+         downgrade/2,
          parse/1]).
 
 -record(test, {
@@ -8,12 +9,15 @@
           f1 = 0,
           f2 = f2_atom,
           f3 = {foo, bar, baz},
-          f4 = "yo mama"
+          f4 = "yo mama",
           f5 = #{}
          }).
 
 new() ->
     #test{}.
+
+downgrade(1, #test{version = 2, f1 = F1, f2 = F2, f3 = F3}) ->
+    {test, 1, F1, F2, F3}.
 
 parse(#test{version = 2, f1 = F1, f2 = F2, f3 = F3, f4 = F4, f5 = F5}) ->
     [{f1, F1}, {f2, F2}, {f3, F3}, {f4, F4}, {f5, F5}];
